5470bece91887743bc89d7b65765257a28a2d999
James <jmchale5555@gmail.com>
2026-03-19 21:04:02 +0000

    simple autoloader improvement

--- app/core/init.php
+++ app/core/init.php
@@ -1,17 +1,27 @@
 <?php
 
-spl_autoload_register(function ($classname)
+spl_autoload_register(function ($className)
 {
-    $classname = explode("\\", $classname);
-    $classname = end($classname);
-    require $filename = "../app/models/" . ucfirst($classname) . ".php";
+    $parts = explode('\\', ltrim($className, '\\'));
+    $shortName = end($parts);
+
+    if (!is_string($shortName) || $shortName === '')
+    {
+        return;
+    }
+
+    $modelFile = __DIR__ . '/../models/' . ucfirst($shortName) . '.php';
+    if (is_file($modelFile))
+    {
+        require_once $modelFile;
+    }
 });
 
-require 'config.php';
-require 'functions.php';
-require 'Database.php';
-require 'Model.php';
-require 'ApiController.php';
-require 'App.php';
-require 'Session.php';
-require 'Request.php';
+require __DIR__ . '/config.php';
+require __DIR__ . '/functions.php';
+require __DIR__ . '/Database.php';
+require __DIR__ . '/Model.php';
+require __DIR__ . '/ApiController.php';
+require __DIR__ . '/App.php';
+require __DIR__ . '/Session.php';
+require __DIR__ . '/Request.php';
